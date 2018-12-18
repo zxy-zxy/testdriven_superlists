c9d77ad7d5182da1159234550bb95d8286d7f2e2
ivan <sinitsyn@awok.ae>
2018-12-18 16:24:20 +0400

    Use forms in all views

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -15,7 +15,7 @@ class NewVisitorTest(FunctionalTest):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -26,7 +26,7 @@ class NewVisitorTest(FunctionalTest):
 
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -36,7 +36,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith start a new todo list
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -61,7 +61,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
--- lists/views.py
+++ lists/views.py
@@ -3,38 +3,30 @@ from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {'form': ItemForm()})
 
 
 def new_list(request):
     if request.method == 'POST':
-        list_ = List.objects.create()
-        new_item_text = request.POST.get('item_text')
-        try:
-            item = Item(text=new_item_text, list=list_)
-            item.full_clean()
-            item.save()
-        except (ValidationError, IntegrityError) as e:
-            list_.delete()
-            error = 'You can\'t have an empty list item'
-            return render(request, 'lists/home.html', {'error': error})
-        # return redirect(f'/lists/{list_.id}/')
-        return redirect(list_)
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            list_ = List.objects.create()
+            Item.objects.create(text=request.POST['text'], list=list_)
+            return redirect(list_)
+        else:
+            return render(request, 'lists/home.html', {'form': form})
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
+    form = ItemForm()
     if request.method == 'POST':
-        try:
-            new_item_text = request.POST.get('item_text')
-            item = Item(text=new_item_text, list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
-        except ValidationError as e:
-            error = 'You can\'t have an empty list item'
-    return render(request, 'lists/list.html', {'list': list_, 'error': error})
+    return render(request, 'lists/list.html', {'list': list_, 'form': form})
