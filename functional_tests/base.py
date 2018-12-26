d32c60f27f62c3a73a63678e7b9068aeb70f7a71
ivan <sinitsyn@awok.ae>
2018-12-26 15:31:41 +0400

    precreate sessions

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -19,6 +19,9 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -42,5 +45,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
+    def wait_to_be_logged_in(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
